d003e78cd0ab0fbe2145222ead1b12d2602d05d5
iamashiqur <getintouch.ashiq@gmail.com>
2024-09-26 15:53:49 +0600

    change the path link

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -3,11 +3,11 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div>
-      <h1 className="text-4xl text-slate-600 mb-5">This is Landing Page</h1>
-      <Link href={'/dashboard/admin'} className="text-5xl block bg-orange-200 mb-5">
+      <h1 className="mb-5 text-4xl text-slate-600">This is Landing Page</h1>
+      <Link href={'/admin'} className="mb-5 block bg-orange-200 text-5xl">
         Go Admin
       </Link>
-      <Link href={'/dashboard/vendor'} className="text-5xl block bg-orange-200">
+      <Link href={'/vendor'} className="block bg-orange-200 text-5xl">
         Go Vendor
       </Link>
     </div>
