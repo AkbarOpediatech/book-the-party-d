8c8dbb91ed7c72728907e1c4f35b077504c5755b
iamashiqur <getintouch.ashiq@gmail.com>
2024-10-25 14:31:19 +0600

    serice page

---
+++ src/app/(landing)/services/components/GridItems.tsx
@@ -0,0 +1,16 @@
+import { specialPackages } from '@/utils'
+import ServiceCard from '../../components/ServiceCard'
+
+const GridItems = () => {
+  return (
+    <div className="grid grid-cols-12 gap-8">
+      {specialPackages.map((items, index) => (
+        <div className="col-span-3" key={index}>
+          <ServiceCard imgSrc={items.img} title={'Book chair arrangements'} review={10} price={100} />
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default GridItems
