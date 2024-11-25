eeb0fd6a1eb2aa5f942073e9fd54ddafdbd56a75
DihanAbir <nahid.muradabir@gmail.com>
2024-11-25 12:05:19 +0600

    added next bundle analyzer

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
+import withBundleAnalyzer from '@next/bundle-analyzer'
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true' // Enable only when ANALYZE=true
+})({
+  reactStrictMode: true, // Your existing Next.js configuration
+  swcMinify: true
+})
 
 export default nextConfig
