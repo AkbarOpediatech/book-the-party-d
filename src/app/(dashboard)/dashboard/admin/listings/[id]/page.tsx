9920dab933dbd3d09d128d6d3a01bc1538b9c2a8
Akbar Hossain <79828975+mdakbarhossain1@users.noreply.github.com>
2024-12-04 18:55:18 +0600

    Responsive Services Vendor listing  (#97)
    * service traking
    
    * fix
    
    * feat: booking status review and cancel btn function
    
    * fixed
    
    * responsive
    
    * res doing
    
    * responsive chats
    
    * responsive services page
    
    * update
    
    * responsive vendor listing services and details pages

--- src/app/(dashboard)/dashboard/admin/listings/[id]/page.tsx
+++ src/app/(dashboard)/dashboard/admin/listings/[id]/page.tsx
@@ -19,38 +19,42 @@ const ListingDetails = () => {
   const { id } = params
 
   return (
-    <div className="bg-white px-7 py-3">
-      <p className="mb-[70px] text-xl font-bold text-clr-36 md:text-2xl">Listing Details {id}</p>
-      <div className="mb-5 grid grid-cols-3 gap-16">
-        <div className="col-span-2">
-          <div className="h-[478px] w-full overflow-hidden rounded-xl">
-            <Image className="w-full object-cover" src={details} alt="pic" />
+    <div className="bg-white px-4 py-3 sm:px-6 lg:px-7">
+      <p className="mb-10 text-lg font-bold text-clr-36 sm:text-xl md:mb-[70px] md:text-2xl">
+        Listing Details {id}
+      </p>
+      <div className="mb-5 grid grid-cols-1 gap-8 md:gap-12 lg:grid-cols-2 lg:gap-16">
+        <div className="col-span-2 md:col-span-1">
+          <div className="w-full overflow-hidden rounded-xl lg:h-[478px]">
+            <Image className="h-auto w-full object-cover lg:h-[478px]" src={details} alt="pic" />
           </div>
         </div>
         <div>
-          <span className="mb-4 inline-block rounded-md bg-clr-16/20 px-2 py-[1px] text-xs font-bold text-clr-16">
+          <span className="mb-4 inline-block rounded-md bg-clr-16/20 px-2 py-1 text-xs font-bold text-clr-16 sm:text-sm">
             Listed
           </span>
-          <p className="mb-4 text-xl font-bold text-clr-36">Wedding Decoration</p>
-          <div className="mb-6 flex items-center gap-1">
+          <p className="mb-4 text-lg font-bold text-clr-36 sm:text-xl">Wedding Decoration</p>
+          <div className="mb-6 flex flex-wrap items-center gap-1">
             <Ratings rating={4.5} />
-            <p className="text-sm text-clr-81"> (11.78kreviews)</p>
+            <p className="text-xs text-clr-81 sm:text-sm"> (11.78kreviews)</p>
           </div>
-          <p className="mb-4 text-sm text-clr-81">Location : Sydney</p>
-          <p className="mb-4 text-xl font-bold text-clr-36 md:text-2xl"> $62.97</p>
+          <p className="mb-4 text-sm text-clr-81 sm:text-base">Location: Sydney</p>
+          <p className="mb-4 text-lg font-bold text-clr-36 sm:text-xl md:text-2xl">$62.97</p>
 
-          <div className="mb-5 inline-flex items-center gap-4 rounded-lg border py-4 pl-3 pr-5 text-clr-ab">
-            <p>$200 security deposit.</p>
-            <InformationCircleIcon className="size-6" />
+          <div className="mb-5 inline-flex flex-wrap items-center gap-4 rounded-lg border py-3 pl-3 pr-5 text-clr-ab sm:py-4">
+            <p className="text-sm sm:text-base">$200 security deposit.</p>
+            <InformationCircleIcon className="h-5 w-5 sm:h-6 sm:w-6" />
           </div>
           <div className="mb-9 border-b border-t border-dashed border-clr-ab/25 py-5">
-            <p className="mb-4 font-semibold text-clr-36">Category</p>
-            <p className="inline-block rounded-lg border py-4 pl-3 pr-5 text-clr-ab">Party Set up</p>
+            <p className="mb-4 font-semibold text-clr-36 sm:text-lg">Category</p>
+            <p className="inline-block rounded-lg border px-4 py-2 text-xs text-clr-ab sm:px-5 sm:py-3 sm:text-sm">
+              Party Set up
+            </p>
           </div>
           <DashboardButton
             name="Remove Item"
             type="button"
-            className="flex w-full justify-center font-bold"
+            className="flex w-full justify-center text-sm font-bold sm:text-base"
           />
         </div>
       </div>
--- src/app/(landing)/profile/page.tsx
+++ src/app/(landing)/profile/page.tsx
@@ -13,31 +13,35 @@ const Profile = () => {
   return (
     <section id="profile">
       <div className="mx-auto max-w-[1440px]">
-        <div className="section-padding">
-          <h1 className="mb-3 font-sora text-2xl font-semibold text-neutral-900 md:text-6xl">Profile</h1>
+        <div className="section-padding p-3">
           <div className="grid grid-cols-1 md:gap-[104px] lg:grid-cols-3">
-            <div className="col-span-1 border-r">
-              <ul className="gap-2 space-y-5 p-4 md:block lg:w-full">
-                {profileMenuItems.map(item => (
-                  <li key={item.label}>
-                    <button
-                      onClick={() => setProfileMenu(item.label)}
-                      className={cn(
-                        'flex items-center gap-3 py-2 text-base font-bold text-clr-96 md:text-nowrap md:text-xl lg:text-2xl xl:text-2xl',
-                        profileMenu === item.label && 'text-clr-fb'
-                      )}
-                    >
-                      <Image
-                        width={26}
-                        height={26}
-                        src={profileMenu === item.label ? item.activeIcon : item.icon}
-                        alt="icon"
-                      />
-                      {item.label}
-                    </button>
-                  </li>
-                ))}
-              </ul>
+            <div className="col-span-1">
+              <div className="h-full rounded-3xl border">
+                <h1 className="mb-3 border-b px-10 py-8 font-sora text-2xl font-semibold text-neutral-900 md:text-6xl lg:mb-8">
+                  Profile
+                </h1>
+                <ul className="m-2 gap-2 space-y-5 rounded-xl border p-4 md:block md:border-0 md:px-12 lg:w-full lg:px-4 lg:pb-8">
+                  {profileMenuItems.map(item => (
+                    <li key={item.label}>
+                      <button
+                        onClick={() => setProfileMenu(item.label)}
+                        className={cn(
+                          'flex items-center gap-3 py-2 text-base font-bold text-clr-96 md:text-nowrap md:text-xl lg:text-2xl xl:text-2xl',
+                          profileMenu === item.label && 'text-clr-fb'
+                        )}
+                      >
+                        <Image
+                          width={26}
+                          height={26}
+                          src={profileMenu === item.label ? item.activeIcon : item.icon}
+                          alt="icon"
+                        />
+                        {item.label}
+                      </button>
+                    </li>
+                  ))}
+                </ul>
+              </div>
             </div>
 
             <div className="col-span-2 m-3">
