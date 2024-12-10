d2aa1d5b15001ce7ed5fd94a663c1d698192b83d
Akbar Hossain <79828975+mdakbarhossain1@users.noreply.github.com>
2024-12-10 10:41:23 +0600

    checkout page flow and delete btn (#98)
    * checkout page flow and delet btn
    
    * checkout page err resolve

--- src/app/(dashboard)/dashboard/vendor/listings/add-new/page.tsx
+++ src/app/(dashboard)/dashboard/vendor/listings/add-new/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 import { usePathname } from 'next/navigation'
+import { usePathname } from 'next/navigation'
 import { useState } from 'react'
 import AddNew from './components/AddNew'
 import ItemList from './components/ItemList'
@@ -7,13 +8,15 @@ import ItemPricing from './components/ItemPricing'
 import SetAvailability from './components/SetAvailability'
 
 const AddNewListing = () => {
-  const [step, setStep] = useState<number>(0)
+  const [step, setStep] = useState<number>(3)
   const router = usePathname()
-
   const isEditListing = router === '/dashboard/vendor/listings/edit-listing'
-
   return (
     <div>
+      {step === 0 && <AddNew setStep={setStep} isEditListing={isEditListing} />}
+      {step === 1 && <ItemList setStep={setStep} isEditListing={isEditListing} />}
+      {step === 2 && <SetAvailability setStep={setStep} isEditListing={isEditListing} />}
+      {step === 3 && <ItemPricing setStep={setStep} isEditListing={isEditListing} />}
       {step === 0 && <AddNew setStep={setStep} isEditListing={isEditListing} />}
       {step === 1 && <ItemList setStep={setStep} isEditListing={isEditListing} />}
       {step === 2 && <SetAvailability setStep={setStep} isEditListing={isEditListing} />}
